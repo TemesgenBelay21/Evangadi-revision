6bd8a9f9ca3b5c98a61eadfcadfe8d33f06565dd
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-03-06 13:20:26 +0100

    examples for understanding name space collisions

---
+++ Phase3/name_space_collision/abebe.js
@@ -0,0 +1,9 @@
+
+var first_var = 1;
+function some_function() {
+    console.log(first_var);
+
+    var second_var = 2;
+    console.log(second_var);
+}
+
---
+++ Phase3/name_space_collision/kebede.js
@@ -0,0 +1,9 @@
+
+var third_variable = 5;
+
+function some_function() {
+   console.log(third_variable);
+   
+   var forth_variable = 9;
+   console.log(forth_variable);
+}
